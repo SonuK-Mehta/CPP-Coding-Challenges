4d36f0ab1cb16c20554691cffd7c28cc7ad236d8
sonum <sonumehta0983@gmail.com>
2023-10-31 09:55:45 +0530

    Armstrong Number

---
+++ Basic-Question/armstrong_num.cpp
@@ -0,0 +1,49 @@
+// Armstrong Number in C++
+//If the sum of individual digits is raised to the power of order if the result is equal to the number itself then it is an Armstrong number.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+cout << "\n\n";
+
+  int num, p, q, rem, sum = 0, count = 0, cnt, add = 1;
+  cout << "Enter a Number: ";
+  cin >> num;
+
+  q = num;
+  p = num;
+
+  while(p != 0){  //122
+    p = p/10;     // 122/10 =12
+    count++;
+  }
+
+  cnt = count;
+  cout << "Number of Digit is: "<< count;
+
+  while(q != 0){
+    rem = q % 10;
+
+      while(cnt != 0){   // 2
+        add = add * rem; // 1 * 5 = 5 // 5 * 5 = 25
+        cnt--;
+      }
+      
+    sum = sum + add; //0+25 =25
+    add = 1;
+    cnt = count;
+    q = q / 10;
+  }
+
+  cout << "\nOutput of Number "<<num<<" is: "<< sum;
+
+  if(num == sum)
+    cout << "\nIt's a Armstrong Number";
+  else 
+    cout << "\nIt's not a Armstrong Number";
+
+
+cout << "\n\n";
+return 0;
+}
