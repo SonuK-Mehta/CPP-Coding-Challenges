f8c6446e35550b0f1f35ed0f216073726ecedbe5
sonum <sonumehta0983@gmail.com>
2023-11-08 09:04:02 +0530

    Armstrong Number in Range

---
+++ Basic-Question/armstrong_num_range.cpp
@@ -0,0 +1,57 @@
+// Armstrong Number in a Range -
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+// number of digits in a number is order
+int order(int x)
+{
+    int len = 0;
+    while (x)
+    {
+        len++;
+        x = x/10;
+    }
+    return len;
+}
+
+void armstrong(int low, int high){
+    
+    for(int num = low; num <= high; num++){
+        
+        int sum = 0, temp, digit, len;
+        temp = num;
+        
+        // function to get order(length)
+        len = order(num);
+        
+        // loop to extract digit, find cube & add to sum
+        while(temp != 0)
+        {
+            // extract digit
+            digit = temp % 10;
+
+            // add power to sum
+            sum = sum + pow(digit,len);;
+            temp /= 10;
+        };
+    
+        if(sum == num)
+            cout << num << " ";
+    }
+}
+
+// Driver Code
+int main ()
+{
+    //variables initialization
+    int low, high ;
+
+    cout << "Enter Two Number: ";
+    cin >> low >> high;
+    // get armstrong numbers
+    armstrong(low, high);
+
+    return 0;
+}
